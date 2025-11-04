0f41e6af19b2911c35e6001bbc8c121b8c9b3b84
AnnieWU026 <wnis026@live.rhul.ac.uk>
2025-11-04 02:39:58 +0000

    Add files via upload

--- 04.media/sketch.js
+++ 04.media/sketch.js
@@ -1,25 +1,29 @@
 let img;
 let sound;
+let started = false;
 
 function preload() {
-img = loadImage('puzzle.webp')
-sound = loadSound('person-walking-in-forest-loop-419565.mp3')
+  img = loadImage('https://upload.wikimedia.org/wikipedia/commons/2/26/Doll_face.jpg');
+  sound = loadSound('https://cdn.pixabay.com/download/audio/2022/03/15/audio_aa6fbbbd5d.mp3?filename=creepy-background-110086.mp3');
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   imageMode(CENTER);
-  sound.loop();
+  background(0);
+  textAlign(CENTER);
+  textSize(24);
+  fill(255);
+  text("start", width/2, height/2);
 }
 
 function draw() {
-  background(0, 20); 
+  if (!started) return;
 
+  background(0, 20);
   let x = width / 2 + random(-10, 10);
   let y = height / 2 + random(-10, 10);
-
   let s = 400 + sin(frameCount * 0.05) * 30;
-
   image(img, x, y, s, s);
 
   if (random(1) < 0.05) {
@@ -29,7 +33,10 @@ function draw() {
 }
 
 function mousePressed() {
-  if (sound.isPlaying()) {
+  if (!started) {
+    started = true;
+    sound.loop();
+  } else if (sound.isPlaying()) {
     sound.pause();
   } else {
     sound.loop();
